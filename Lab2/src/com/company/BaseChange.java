11aea4ed23311c19aa3d15f4137651ad08a52707
LHS Compsci <lhscompsci@lh-l703-15-7368.ci.lexington.ma.us>
2016-09-29 14:25:26 -0400

    addition done and tested, multiplication started

--- Lab2/src/com/company/BaseChange.java
+++ Lab2/src/com/company/BaseChange.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,44 +13,42 @@ public class BaseChange {
 //        System.out.println("Please enter your desired base:");
 //        System.out.println(findDigit(10, 2));
 //        System.out.println(countDigits(10000));
-        System.out.println(addition(10, 1, 2));
+        System.out.println((addition(1234, 1234, 5)));
     }
-
-    public static int addition(int num1, int num2, int base) {
-        int finalNum = 0, carryOver = 0, loopLength;
+    public static void multiplication(int num1, int num2, int base) {
+    int loopLength, finalNum = 0, carryOver = 0;
         int numberOfDigits1 = countDigits(num1);
         int numberOfDigits2 = countDigits(num2);
-
-            int number1[] = new int[numberOfDigits1];
-            int number2[] = new int[numberOfDigits2];
-
-        for(int j = 1; j <= 2; j++) {
-            if (j == 1)
-                loopLength = (numberOfDigits1);
-            else
-                loopLength = (numberOfDigits2);
-//            System.out.println(loopLength);
-
-            for (int i = loopLength - 1; i >= 0; i--) {
-//                System.out.println(loopLength +  " - looplength | countDigit - " + countDigits(num1));
-                if (j == 1) {
-//                    System.out.println("loop fam");
-                    number1[i] = findDigit(num1, loopLength - i);
-                }
-                else
-                    number2[i] = findDigit(num2, loopLength - i);
-            }
-            System.out.println(Arrays.toString(number1));
-        }
 
         if (numberOfDigits1 > numberOfDigits2)
             loopLength = numberOfDigits1;
         else
             loopLength = numberOfDigits2;
 
+        for(int i = 1; i <= (loopLength + 1); i++) {
+
+        }
+    //nested loop that cycles through second digit in big loop, and first digit top loop
+    }
+    public static int addition(int num1, int num2, int base) {
+        int loopLength, finalNum = 0, carryOver = 0;
+        int numberOfDigits1 = countDigits(num1);
+        int numberOfDigits2 = countDigits(num2);
+
+        if (numberOfDigits1 > numberOfDigits2)
+            loopLength = numberOfDigits1;
+        else
+            loopLength = numberOfDigits2;
+
+        for(int i = 1; i <= loopLength+1; i++) {
+            int temp1 = findDigit(num1, i);
+            int temp2 = findDigit(num2, i);
+//            System.out.println(carryOver);
+            finalNum += ((temp1 + temp2 + carryOver) % base) * power(10, (i-1));
+            carryOver = (temp1 + temp2 +carryOver) / base;
+        }
         return finalNum;
     }
-
     public static int findDigit(int number, int position) {
         for(int i = 1; i < position; i++)
             number /= 10;
