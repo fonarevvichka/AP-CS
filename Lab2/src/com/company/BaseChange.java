be502a208f67145da3f324c200969dd2593fac95
Vichka Fonarev <fonarev.vichka@gmail.com>
2016-10-02 13:51:02 -0400

    everything should be done, menu and output formatting is done, final check tbd

--- Lab2/src/com/company/BaseChange.java
+++ Lab2/src/com/company/BaseChange.java
@@ -9,15 +9,42 @@ public class BaseChange {
     public static void main(String[] args) {
 
         Scanner cin = new Scanner(System.in);
-
-//        System.out.println("Please enter your desired base:");
-//        System.out.println(findDigit(10, 2));
-//        System.out.println(countDigits(10000));
-//        System.out.println((addition(1234, 1234, 5)));
-        multiplication(101, 1, 2);
+        for (int i = 1; i < 11; i++) {
+            System.out.println("\n" + "Please enter your desired base:");
+            String stringBase = new String(cin.next());
+            int base = baseConversion(stringBase);
+            if (base != -1) {
+                System.out.println("Please enter your first number");
+                int num1 = cin.nextInt();
+                System.out.println("Please enter your second number");
+                int num2 = cin.nextInt();
+                System.out.println("Please enter the desired operation ('+' and '*' only)");
+                String operation = cin.next();
+                System.out.println("Output " + i + ":");
+                if (operation.equals("*")) {
+                    System.out.println(max(num1, num2));
+                    System.out.println("* " + min(num1, num2));
+                    for(int z = 0; z <= max(countDigits(num1), countDigits(num2)) + 2; z++)
+                        System.out.print("-");
+                    System.out.print("\n");
+                    multiplication(num1, num2, base);
+                } else if (operation.equals("+")) {
+                   System.out.print(max(num1, num2));
+                    System.out.println("\n" + operation + " " + min(num1, num2));
+                    for (int j = 0; j <= countDigits(max(num1, num2)); j++)
+                        System.out.print("=");
+                    System.out.println("\n" + addition(num1, num2, base));
+                } else
+                    System.out.println("Invalid operation, please try again");
+            } else {
+                System.out.println("Inavlid base, please try gain");
+                i--;
+            }
+        }
     }
+
     public static void multiplication(int num1, int num2, int base) {
-        int loopLength, finalNum = 0, carryOver = 0, shortLoop;
+        int loopLength, finalNum = 0, carryOver = 0, shortLoop, finalNumCopy = 0;
         int numberOfDigits1 = countDigits(num1);
         int numberOfDigits2 = countDigits(num2);
         boolean firstNum = false;
@@ -26,29 +53,44 @@ public class BaseChange {
             loopLength = numberOfDigits1;
             shortLoop = numberOfDigits2;
             firstNum = true;
-//            System.out.println(loopLength + " " + shortLoop + " " + firstNum);
-        }
-        else {
+        } else {
             loopLength = numberOfDigits2;
             shortLoop = numberOfDigits1;
         }
-        for(int i = 1; i <= (loopLength + 1); i++) {
-            int temp1 = findDigit(num1, i);
-            if(!firstNum)
-                temp1 = findDigit(num2, i);
-//System.out.println(temp1);
-            for(int j = 1; j <= shortLoop + 1; j++) {
-                int temp2 = findDigit(num2, j);
-                if(!firstNum)
-                    temp2 = findDigit(num1, j);
-                finalNum += (((temp1 * temp2) + carryOver) % base) * (int) power(10, i+j - 2);
+
+        for (int i = 1; i <= (shortLoop + 1); i++) {
+            int temp2 = findDigit(num2, i);
+            if (!firstNum)
+                temp2 = findDigit(num1, i);
+            for (int j = 1; j <= loopLength + 1; j++) {
+                int temp1 = findDigit(num1, j);
+                if (!firstNum)
+                    temp1 = findDigit(num2, j);
+                finalNum += (((temp1 * temp2) + carryOver) % base) * (int) power(10, i + j - 2);
+                finalNumCopy += (((temp1 * temp2) + carryOver) % base) * (int) power(10, i+ j - 2);
                 carryOver = ((((temp1 * temp2) + carryOver) / base));
             }
-//           System.out.println(i);
+            if(i != (shortLoop + 1))
+                System.out.println(finalNumCopy / (int) power(10, i-1));
+//            finalNumCopy = 0;
+//            finalNumCopy =- finalNum;
         }
+        for (int j = 0; j <= countDigits(Math.max(num1, num2)) + 1; j++)
+            System.out.print("=");
+        System.out.println("\n");
         System.out.println(finalNum);
-        //install karabiner
-    //nested loop that cycles through second digit in big loop, and first digit top loop
+    }
+    public static int baseConversion(String base) {
+        if (base.equals("human"))
+            return 10;
+        else if (base.equals("elf"))
+            return 8;
+        else if (base.equals("dwarf"))
+            return 5;
+        else if (base.equals("wizard"))
+            return 2;
+        else
+            return -1;
     }
     public static int addition(int num1, int num2, int base) {
         int loopLength, finalNum = 0, carryOver = 0;
@@ -60,51 +102,61 @@ public class BaseChange {
         else
             loopLength = numberOfDigits2;
 
-        for(int i = 1; i <= loopLength+1; i++) {
+        for (int i = 1; i <= loopLength + 1; i++) {
             int temp1 = findDigit(num1, i);
             int temp2 = findDigit(num2, i);
 //            System.out.println(carryOver);
-            finalNum += ((temp1 + temp2 + carryOver) % base) * power(10, (i-1));
-            carryOver = (temp1 + temp2 +carryOver) / base;
+            finalNum += ((temp1 + temp2 + carryOver) % base) * power(10, (i - 1));
+            carryOver = (temp1 + temp2 + carryOver) / base;
         }
         return finalNum;
     }
     public static int findDigit(int number, int position) {
-        for(int i = 1; i < position; i++)
+        for (int i = 1; i < position; i++)
             number /= 10;
 
-        return number%10;
+        return number % 10;
     }
     public static double power(double base, int exponent) {
         double finalNum = base;
         boolean change = false;
 
-        if(exponent < 0) {
+        if (exponent < 0) {
             exponent *= -1;
             change = true;
         }
 
-        if(exponent != 0) {
+        if (exponent != 0) {
             for (int i = 1; i < exponent; i++) {
                 finalNum *= base;
             }
-            if(change == true)
-                return (1/finalNum);
+            if (change == true)
+                return (1 / finalNum);
             else
                 return finalNum;
-        }
-
-        else
+        } else
             return 1;
     }
     public static int countDigits(double num) {
         int numberLength = 1;
         int numCopy = (int) num;
 
-        while((numCopy /= 10) != 0)
+        while ((numCopy /= 10) != 0)
             numberLength++;
 
 //        System.out.println(numberLength);
         return numberLength;
+    }
+    public static int max(int num1, int num2) {
+        if (num1 > num2)
+            return num1;
+        else
+            return num2;
+    }
+    public static int min(int num1, int num2) {
+        if (num1 > num2)
+            return num2;
+        else
+            return num1;
     }
 }
