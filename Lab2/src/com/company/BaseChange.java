2653965eb29860492160b98793d1f1b1551cc890
Vichka Fonarev <fonarev.vichka@gmail.com>
2016-10-02 22:18:58 -0400

    rewrote/fixed multiplication - fixed carry over issue, tested, one more run to go

--- Lab2/src/com/company/BaseChange.java
+++ Lab2/src/com/company/BaseChange.java
@@ -27,7 +27,7 @@ public class BaseChange {
                     for(int z = 0; z <= max(countDigits(num1), countDigits(num2)) + 2; z++)
                         System.out.print("-");
                     System.out.print("\n");
-                    multiplication(num1, num2, base);
+                    multiplicationV2(num1, num2, base);
                 } else if (operation.equals("+")) {
                    System.out.print(max(num1, num2));
                     System.out.println("\n" + operation + " " + min(num1, num2));
@@ -41,44 +41,6 @@ public class BaseChange {
                 i--;
             }
         }
-    }
-
-    public static void multiplication(int num1, int num2, int base) {
-        int loopLength, finalNum = 0, carryOver = 0, shortLoop, finalNumCopy = 0;
-        int numberOfDigits1 = countDigits(num1);
-        int numberOfDigits2 = countDigits(num2);
-        boolean firstNum = false;
-
-        if (numberOfDigits1 > numberOfDigits2) {
-            loopLength = numberOfDigits1;
-            shortLoop = numberOfDigits2;
-            firstNum = true;
-        } else {
-            loopLength = numberOfDigits2;
-            shortLoop = numberOfDigits1;
-        }
-
-        for (int i = 1; i <= (shortLoop + 1); i++) {
-            int temp2 = findDigit(num2, i);
-            if (!firstNum)
-                temp2 = findDigit(num1, i);
-            for (int j = 1; j <= loopLength + 1; j++) {
-                int temp1 = findDigit(num1, j);
-                if (!firstNum)
-                    temp1 = findDigit(num2, j);
-                finalNum += (((temp1 * temp2) + carryOver) % base) * (int) power(10, i + j - 2);
-                finalNumCopy += (((temp1 * temp2) + carryOver) % base) * (int) power(10, i+ j - 2);
-                carryOver = ((((temp1 * temp2) + carryOver) / base));
-            }
-            if(i != (shortLoop + 1))
-                System.out.println(finalNumCopy / (int) power(10, i-1));
-//            finalNumCopy = 0;
-//            finalNumCopy =- finalNum;
-        }
-        for (int j = 0; j <= countDigits(Math.max(num1, num2)) + 1; j++)
-            System.out.print("=");
-        System.out.println("\n");
-        System.out.println(finalNum);
     }
     public static int baseConversion(String base) {
         if (base.equals("human"))
@@ -158,5 +120,32 @@ public class BaseChange {
             return num2;
         else
             return num1;
+    }
+    public static void multiplicationV2(int num1, int num2, int base) {
+        int outsideLoop = min(countDigits(num1), countDigits(num2)) + 1;
+        int nestedLoop = max(countDigits(num1), countDigits(num2)) + 1;
+
+        int finalNum = 0; int numToPrint = 0, carryOver = 0;
+
+        int smaller = min(num1, num2);
+        int larger = max(num1, num2);
+
+        for(int i = 1; i <= outsideLoop; i++) {
+            int smallerTemp = findDigit(smaller, i);
+            numToPrint = 0;
+            for(int j = 1; j <= nestedLoop; j++) {
+                int largerTemp = findDigit(larger, j);
+                finalNum = addition(((smallerTemp * largerTemp + carryOver) % base) * (int) power(10, ((i + j) - 2)), finalNum, base);
+                numToPrint += ((smallerTemp * largerTemp + carryOver) % base) * (int) power(10, j - 1);
+                carryOver = ((smallerTemp * largerTemp + carryOver) / base);
+            }
+            if (i < outsideLoop)
+                System.out.println(numToPrint);
+        }
+        for (int j = -1; j <= outsideLoop; j++)
+            System.out.print("=");
+        System.out.print("\n");
+        System.out.println(finalNum);
+
     }
 }
