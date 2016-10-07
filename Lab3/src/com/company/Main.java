34290174986667c6a341865c4c0a508cfa1233fe
Vichka Fonarev <fonarev.vichka@gmail.com>
2016-10-06 23:50:12 -0400

    threes, twos, and both reversal work

--- Lab3/src/com/company/Main.java
+++ Lab3/src/com/company/Main.java
@@ -2,46 +2,40 @@ package com.company;
 
 public class Main {
 
-    public static String letters (char lastLetter) {
+    public static String letters(char lastLetter) {
         String finalString = "";
-        if (lastLetter == 'a') {
+        if (lastLetter == 'a')
            return "a";
-        }
-
-        else {
-//            System.out.println("final string " + finalString);
-            return finalString = lastLetter + letters((char) ((int) lastLetter - 1));
-        }
-
+        else
+            return lastLetter + letters((char) ((int) lastLetter - 1));
     }
-
-    public int twos (int number) {
-        int numberOfFactors = 0;
-
-        return numberOfFactors;
+    public int twos(int num) {
+        if ((num % 2) != 0)
+            return 0;
+        else
+            return 1 + twos(num/2);
     }
-
-    public boolean powerOf3 (int number) {
-       return true;
+    public boolean powerOf3(int num) {
+        if ((num % 3) != 0)
+            return false;
+        else if (num >1)
+            return powerOf3(num/3);
+        else
+            return true;
     }
-
-    public static int reverse (int number) {
-        String temp = Integer.toString(number);
-        int lengthOfNum = temp.length();
-//        System.out.println(lengthOfNum);
-        int reversedNumber = 0;
-        if (number < 10)
-            return number;
-
-        else {
-            reversedNumber += Math.pow(10, (lengthOfNum - 1)) * (number % 10);
-            System.out.println(number/10);
-            reverse(number/10);
-        }
-       return reversedNumber;
+    public static int reverse(int num) {
+        if (num < 10)
+            return num;
+        else
+            return ((num % 10) * power(num)) + reverse(num/10);
     }
     public static void main(String[] args) {
-	// write your code here
         System.out.println(reverse(1234));
+    }
+    public static int power(int num) {
+        if (num < 10)
+            return 1;
+        else
+            return 10 * power(num/10);
     }
 }
