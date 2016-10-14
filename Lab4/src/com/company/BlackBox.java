efc0b6af144ae628435a7e21da97069598311dad
LHS Compsci <lhscompsci@lh-l703-15-7368.ci.lexington.ma.us>
2016-10-14 12:41:33 -0400

    changed how direction works to a single varaible, not tested

--- Lab4/src/com/company/BlackBox.java
+++ Lab4/src/com/company/BlackBox.java
@@ -15,93 +15,80 @@ public class BlackBox {
         
         if(shootLocation < 10) {
             position[0] = shootLocation;
-            position[1] = 0;
-            position[2] = 1;
-            position[3] = 1;
+            position[1] = 9;
+            position[2] = 3;
         } else if(shootLocation > 9 && 20 > shootLocation) {
             position[0] = 0;
-            position[1] = shootLocation - 10;
+            position[1] = 10 - (19 - shootLocation);
             position[2] = 0;
-            position[3] = 1;
         } else if(shootLocation > 19 && 30 > shootLocation) {
             position[0] = shootLocation - 20;
-            position[1] = 9;
+            position[1] = 3;
             position[2] = 1;
-            position[3] = 0;
         } else {
             position[0] = 9;
             position[1] = shootLocation - 30;
-            position[2] = 0;
-            position[3] = 0;
+            position[2] = 2;
         }
         return position;
     }
     public static int shootLaser(int shootLocation, char[][] blackBox) {
         int endLocation = 0;
-        int[] position = moveLaser(initPositionFinder(shootLocation));
+        int[] position = initPositionFinder(shootLocation);
 
+        System.out.println(position[0]);
+        System.out.println(position[1]);
         int xPosition = position[0];
         int yPosition = position[1];
-        int vertical = position[2];
-        int right = position[3];
-        int up = position[3];
+        int direction = position[2];
 
+//        blackBox[0][4] = '\\';
         while(xPosition >= 0 && xPosition <= 9 && yPosition >= 0 && yPosition <= 9) {
-        //--------------- check --------------------//
-            if(blackBox[xPosition][yPosition] == '\\') {
-                if(vertical == 1) {
-                    if (up == 1) {
-                        right = 0;
-                        vertical = 0;
-                    } else {
-                        right = 1;
-                        vertical = 0;
-                    }
-                } else {
-                    if (right == 1) {
-                        up = 0;
-                        vertical = 1;
-                    } else {
-                        up = 1;
-                        vertical = 1;
-                    }
+            System.out.println(blackBox[xPosition][yPosition]);
+            //--------------- check --------------------//
+            if (blackBox[xPosition][yPosition] == '\\') {
+                switch(direction) {
+                    case(0):
+                        direction = 1;
+                        break;
+                    case(1):
+                        direction = 0;
+                        break;
+                    case(2):
+                        direction = 3;
+                        break;
+                    case(3):
+                        direction = 2;
+                        break;
                 }
+
             }
 
-            if(blackBox[xPosition][yPosition] == '/') {
-                if(vertical == 1) {
-                    if (up == 1) {
-                        right = 1;
-                        vertical = 0;
-                    } else {
-                        right = 0;
-                        vertical = 0;
-                    }
-                } else {
-                    if (right == 1) {
-                        up = 1;
-                        vertical = 1;
-                    } else {
-                        up = 0;
-                        vertical = 1;
-                    }
+            if (blackBox[xPosition][yPosition] == '/') {
+                switch(direction) {
+                    case(0):
+                        direction = 3;
+                        break;
+                    case(1):
+                        direction = 2;
+                        break;
+                    case(2):
+                        direction = 1;
+                        break;
+                    case(3):
+                        direction = 0;
+                        break;
                 }
             }
-            position[2] = vertical;
-            position[3] = right;
+                //--------------- check --------------------//
+                moveLaser(position);
 
-        //--------------- check --------------------//
-            moveLaser(position);
-
-            xPosition = position[0];
-            yPosition = position[1];
-            vertical = position[2];
-            right = position[3];
-            up = position[3];
+                xPosition = position[0];
+                yPosition = position[1];
         }
-       System.out.println(xPosition);
-       System.out.println(yPosition);
-        return endLocation;
+            System.out.println(xPosition);
+            System.out.println(yPosition);
+            return endLocation;
     }
     public static char[][] arrayCreate() {
         char blackBox[][] = new char[10][10];
@@ -162,32 +149,31 @@ public class BlackBox {
 //------------ bottom row numers -------------//
     }
     public static int[] moveLaser(int[] position) {
-        int sign = -1;
-
         int xPosition = position[0];
         int yPosition = position[1];
-        int vertical = position[2]; // 0 means horizontal, 1 is vertical;
-        int up = position[3]; // 0 means down, 1 means up
-        int right = position[3]; // 0 means left, 1 means right
-
-        if (up != 0)
-            sign = 1;
+        int direction = position[2]; // 0 - right, 1 -  down, 2 - left, 3 - up;
 
         //--------------- laser move ---------------//
-        if (vertical != 0) {
-            yPosition += (1 * sign);
-        } else {
-            xPosition += (1 * sign);
-           System.out.println("xPosition - " + xPosition);
+        switch(direction) {
+            case(0):
+                xPosition++;
+                break;
+            case(1):
+                yPosition--;
+                break;
+            case(2):
+                xPosition--;
+                break;
+            case(3):
+                yPosition++;
+                break;
         }
+
         //--------------- laser move ---------------//
         position[0] = xPosition;
         position[1] = yPosition;
 
-        System.out.print("vertical- " + vertical);
-        System.out.print(" | up - " + up);
-        System.out.print(" | right - " + right);
-        System.out.print("\n");
+//        System.out.print("\n");
         return position;
     }
 }
