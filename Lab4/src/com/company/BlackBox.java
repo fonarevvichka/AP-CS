93d97f57015bda483b83754549829e756a3f1710
Vichka Fonarev <fonarev.vichka@gmail.com>
2016-10-14 23:20:39 -0400

    fully tested and working. Change stat formating, and better exit dialogue

--- Lab4/src/com/company/BlackBox.java
+++ Lab4/src/com/company/BlackBox.java
@@ -1,107 +1,89 @@
 package com.company;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class BlackBox {
 
     public static void main(String[] args) {
-        char array[][] = arrayCreate();
-        printArray(array);
+        Scanner cin = new Scanner(System.in);
+        char blackBox[][] = arrayCreate(false);
+        char whiteBox[][] = arrayCreate(true);
+        int position[] = initPositionFinder(0);
+        boolean stop = false;
+        int trialNum = 0;
 
-       shootLaser(14, array);
+//        printArray(blackBox);
+        while(!stop) {
+            System.out.println("Trial Number: " + trialNum++);
+            printArray(whiteBox);
+           switch (menu(cin)) {
+               case (1):
+                    laserShootCaller(cin, position, blackBox);
+                   break;
+               case (2):
+                   mirrorGuess(cin, position, blackBox, whiteBox);
+                   break;
+               case (0):
+                   stop = true;
+                   System.out.println("The black box looked like this: ");
+                   printArray(blackBox);
+                   break;
+           }
+       }
     }
     public static int[] initPositionFinder(int shootLocation) {
-        int[] position = new int[4];
+        int[] position = new int[3];
         
         if(shootLocation < 10) {
-            position[0] = shootLocation;
-            position[1] = 9;
+            position[1] = shootLocation;
+            position[0] = 9;
             position[2] = 3;
         } else if(shootLocation > 9 && 20 > shootLocation) {
-            position[0] = 0;
-            position[1] = 10 - (19 - shootLocation);
+            position[1] = 0;
+            position[0] = 19 - shootLocation;
             position[2] = 0;
         } else if(shootLocation > 19 && 30 > shootLocation) {
-            position[0] = shootLocation - 20;
-            position[1] = 3;
+            position[1] = shootLocation - 20;
+            position[0] = 0;
             position[2] = 1;
         } else {
-            position[0] = 9;
-            position[1] = shootLocation - 30;
+            position[1] = 9;
+            position[0] = shootLocation - 30;
             position[2] = 2;
         }
         return position;
     }
-    public static int shootLaser(int shootLocation, char[][] blackBox) {
-        int endLocation = 0;
-        int[] position = initPositionFinder(shootLocation);
-
-        System.out.println(position[0]);
-        System.out.println(position[1]);
-        int xPosition = position[0];
-        int yPosition = position[1];
-        int direction = position[2];
-
-//        blackBox[0][4] = '\\';
-        while(xPosition >= 0 && xPosition <= 9 && yPosition >= 0 && yPosition <= 9) {
-            System.out.println(blackBox[xPosition][yPosition]);
+    public static int shootLaser(char[][] blackBox, int[] position) {
+        int yPosition = position[0];
+        int xPosition = position[1];
+        if(xPosition >= 0 && xPosition <= 9 && yPosition >= 0 && yPosition <= 9) {
             //--------------- check --------------------//
-            if (blackBox[xPosition][yPosition] == '\\') {
-                switch(direction) {
-                    case(0):
-                        direction = 1;
-                        break;
-                    case(1):
-                        direction = 0;
-                        break;
-                    case(2):
-                        direction = 3;
-                        break;
-                    case(3):
-                        direction = 2;
-                        break;
-                }
+            if (blackBox[yPosition][xPosition] == '\\')
+                position[2] = backSlash(position[2]);
+            if (blackBox[yPosition][xPosition] == '/')
+                position[2] = forwardSlash(position[2]);
+            //--------------- check --------------------//
+            position = moveLaser(position);
+            yPosition = position[0];
+            xPosition = position[1];
 
-            }
-
-            if (blackBox[xPosition][yPosition] == '/') {
-                switch(direction) {
-                    case(0):
-                        direction = 3;
-                        break;
-                    case(1):
-                        direction = 2;
-                        break;
-                    case(2):
-                        direction = 1;
-                        break;
-                    case(3):
-                        direction = 0;
-                        break;
-                }
-            }
-                //--------------- check --------------------//
-                moveLaser(position);
-
-                xPosition = position[0];
-                yPosition = position[1];
+            return shootLaser(blackBox, position); // can i just get rid of the return part, and just call it again, is this actually recursive
         }
-            System.out.println(xPosition);
-            System.out.println(yPosition);
-            return endLocation;
+        return finalPosition(position);
     }
-    public static char[][] arrayCreate() {
+    public static char[][] arrayCreate(boolean clean) {
         char blackBox[][] = new char[10][10];
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                blackBox[i][j] = '.';
+                blackBox[j][i] = '.';
             }
         }
-
-        for (int i = 0; i < 10; i++)
-            generateMirror(blackBox);
-
+        if(!clean) {
+            for (int i = 0; i < 10; i++)
+                generateMirror(blackBox);
+        }
         return blackBox;
     }
     public static char[][] generateMirror(char[][] blackBox) {
@@ -110,17 +92,21 @@ public class BlackBox {
         int mirrorLocationY = rand.nextInt(10);
         int mirrorType = rand.nextInt(2);
 
-        if (blackBox[mirrorLocationX][mirrorLocationY] == '/' || blackBox[mirrorLocationX][mirrorLocationY] == '\\')
+        if (blackBox[mirrorLocationY][mirrorLocationX] == '/' || blackBox[mirrorLocationY][mirrorLocationX] == '\\')
             generateMirror(blackBox);
         else {
-            if (mirrorType == 1)
-                blackBox[mirrorLocationX][mirrorLocationY] = '/';
-            else
-                blackBox[mirrorLocationX][mirrorLocationY] = '\\';
+            if (mirrorType == 1) {
+                blackBox[mirrorLocationY][mirrorLocationX] = '/';
+//                System.out.println(mirrorLocationX + " - x | y - " + mirrorLocationY);
+            }
+            else{
+                blackBox[mirrorLocationY][mirrorLocationX] = '\\';
+//                System.out.println(mirrorLocationX + " - x | y - " + mirrorLocationY);
+            }
         }
         return blackBox;
     }
-    public static void printArray(char[][] blackBox) {
+    public static void printArray(char[][] whiteBox) {
 
 //------------ top row numbers ---------------//
         System.out.print("   ");
@@ -133,7 +119,7 @@ public class BlackBox {
         for (int i = 0; i < 10; i++) {
             System.out.print(19 - i + " ");
             for (int j = 0; j < 10; j++) {
-                System.out.print(blackBox[i][j] + " ");
+                System.out.print(whiteBox[i][j] + " ");
             }
             System.out.print(30 + i + " ");
             System.out.print("\n");
@@ -149,8 +135,8 @@ public class BlackBox {
 //------------ bottom row numers -------------//
     }
     public static int[] moveLaser(int[] position) {
-        int xPosition = position[0];
-        int yPosition = position[1];
+        int yPosition = position[0];
+        int xPosition = position[1];
         int direction = position[2]; // 0 - right, 1 -  down, 2 - left, 3 - up;
 
         //--------------- laser move ---------------//
@@ -159,21 +145,130 @@ public class BlackBox {
                 xPosition++;
                 break;
             case(1):
-                yPosition--;
+                yPosition++;
                 break;
             case(2):
                 xPosition--;
                 break;
             case(3):
-                yPosition++;
+                yPosition--;
                 break;
         }
-
-        //--------------- laser move ---------------//
-        position[0] = xPosition;
-        position[1] = yPosition;
+    //--------------- laser move ---------------//
+        position[0] = yPosition;
+        position[1] = xPosition;
 
 //        System.out.print("\n");
         return position;
+    }
+    public static int backSlash(int direction) {
+        switch(direction) {
+            case(0):
+                direction = 1;
+                break;
+            case(1):
+                direction = 0;
+                break;
+            case(2):
+                direction = 3;
+                break;
+            case(3):
+                direction = 2;
+                break;
+        }
+        return direction;
+    }
+    public static int forwardSlash(int direction) {
+        switch(direction) {
+            case(0):
+                direction = 3;
+                break;
+            case(1):
+                direction = 2;
+                break;
+            case(2):
+                direction = 1;
+                break;
+            case(3):
+                direction = 0;
+                break;
+        }
+        return direction;
+    }
+    public static int finalPosition(int[] position) {
+        int xPosition = position[1];
+        int yPosition = position[0];
+        int direction = position[2];
+        int finalNum = 0;
+
+        switch(direction) {
+            case(0):
+                finalNum = yPosition + 30;
+                break;
+            case(1):
+                finalNum = xPosition;
+                break;
+            case(2):
+               finalNum = 19 - yPosition;
+                break;
+            case(3):
+                finalNum = xPosition + 20;
+                break;
+        }
+        return finalNum;
+    }
+    public static int menu(Scanner cin) {
+        System.out.println("(1) Shoot a laser" + "\n" +
+                          "(2) Guess at a mirror location" + "\n" +
+                          "(0) Quit the game" + "\n");
+        System.out.println("Enter choice: ");
+        return cin.nextInt();
+    }
+    public static boolean locationChecker(char[][] blackBox, char[][] whiteBox, int[] position) {
+        char toCompare = blackBox[position[0]][position[1]];
+        if (toCompare == '\\' || toCompare == '/') {
+            whiteBox[position[0]][position[1]] = toCompare;
+            return true;
+        }
+        return false;
+    }
+    public static void mirrorGuess(Scanner cin, int[] position, char [][] blackBox, char[][] whiteBox) {
+        System.out.println("Please enter the x coordinate of your guess: ");
+        position[1] = cin.nextInt();
+        if((position[1] > -1 && position[1] < 10) || (position[1] > 19 && position[1] < 30)) {
+            if (position[1] > 19)
+                position[1] = -20;
+            System.out.println("Please enter the y coordinate of your guess: ");
+            position[0] = cin.nextInt();
+            if ((position[0] > 9 && position[0] < 20) || (position[0] > 29 && position[0] < 40)) {
+                if (position[0] > 29)
+                    position[0] = position[0] -30;
+                else
+                    position[0] = 19 - position[0];
+                //--------------- checking mirror guess ----------------//
+                if (locationChecker(blackBox, whiteBox, position))
+                    System.out.println("Your guess was correct.");
+                else
+                    System.out.println("Your guess was incorrect.");
+                //--------------- checking mirror guess ----------------//
+            } else {
+                System.out.println("Invalid input, please try again");
+                mirrorGuess(cin, position, blackBox, whiteBox);
+            } } else{
+                System.out.println("Invalid input, please try again");
+                mirrorGuess(cin, position, blackBox, whiteBox);
+            }
+
+    }
+    public static void laserShootCaller(Scanner cin, int[] position, char[][] blackBox) {
+        System.out.println("Please enter the number you would like to shoot from: ");
+        int laserOrigin = cin.nextInt();
+        if (laserOrigin > -1 && 40 > laserOrigin) {
+            position = initPositionFinder(laserOrigin);
+            System.out.println("The laser exited at: " + shootLaser(blackBox, position));
+        } else {
+           System.out.println("Laser origin out of range, please try again.");
+            laserShootCaller(cin, position, blackBox);
+        }
     }
 }
