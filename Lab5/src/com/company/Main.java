3a507a7a2ba3551ac13eb330ff37cdf26722aee3
Vichka Fonarev <fonarev.vichka@gmail.com>
2016-10-27 01:04:08 -0400

    all done except formatting and case when zeroes are in the beggingin

--- Lab5/src/com/company/Main.java
+++ Lab5/src/com/company/Main.java
@@ -12,16 +12,12 @@ public class Main {
         fillArray(numbers, size, cin);
 
         printArray(numbers, size);
-        // find value caller
-
-        String choice = "";
-        do {
-            System.out.println("What entry are you looking for? ");
-            found = findValue(numbers, size, cin.nextInt());
-            if
-            System.out.println("Do you want to continue searching (yes/no)?: ");
-        } while(choice.equals("yes"));
-        // find value caller
+//        findValue(numbers, size, cin);
+        System.out.print("\n");
+        System.out.print("\n");
+//        rotate(numbers, size, cin);a
+        size = deleteZeroes(numbers, size);
+        printArray(numbers, size);
     }
 
     public static void printArray(int numbers[], int arrayLength) {
@@ -46,16 +42,88 @@ public class Main {
     public static void printSize(int size) {
         System.out.println("Size of array: " + size);
     }
-    public static void findValue(int numbers[], int size, int query) {
-        boolean found = false;
-        int placeFound = 0;
+    public static void findValue(int numbers[], int size, Scanner cin) {
+        System.out.print("\n");
+        do {
+           System.out.println("What entry? ");
+            int query = cin.nextInt();
+            boolean found = false;
+            int placeFound = 0;
+            for(int i = 0; i < size; i++) {
+                if (numbers[i] == query) {
+                    found = true;
+                    placeFound = i;
+                    i = size;
+                }
+            }
+            if(found) {
+                System.out.println("status: found at position: " + placeFound);
+            } else {
+                System.out.println("status: not found");
+            }
+            System.out.println("Do you want to continue searching (yes/no)?: ");
+        } while(cin.next().equals("yes"));
+    }
+    public static void smallest(int numbers[], int size) {
+        int smallestValue = 666;
+        int tempValue, changeAdd = 0;
         for(int i = 0; i < size; i++) {
-            if(numbers[i] == query) {
-                found = true;
-                placeFound = i;
-                i = size;
+            tempValue = numbers[i];
+            if(tempValue < smallestValue) {
+                smallestValue = tempValue;
+                changeAdd = i;
             }
         }
-        return false;
+        if(changeAdd != 0) {
+            numbers[changeAdd] = numbers[0];
+            numbers[0] = smallestValue;
+        }
+    }
+    public static void rotate(int numbers[], int size, Scanner cin) {
+        System.out.println("How many steps? ");
+        int steps = cin.nextInt();
+        int temp = 0, temp2 = 0;
+        if(steps > 0) {
+            for(int i = 0; i < steps; i++) {
+                for(int k = 0; k < size; k++) {
+                    if(k == 0){
+                        temp = numbers[k];
+                        numbers[k] = numbers[size -1];
+                   } else {
+                        temp2 = temp;
+                        temp = numbers[k];
+                        numbers[k] = temp2;
+                    }
+                }
+            }
+        } else {
+            for(int i = size - 1; i >= 0; i--) {
+                if(i == size-1) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[0];
+                } else {
+                    temp2 = temp;
+                    temp = numbers[i];
+                    numbers[i] = temp2;
+                }
+            }
+        }
+    }
+    public static int deleteZeroes(int numbers[], int size) {
+       int newSize = size;
+        for(int i = 0; i < size; i++) {
+           if(numbers[i] == 0) {
+               if(i == size -1) {
+                   newSize--;
+                   i = size;
+               } else {
+                   numbers[i] = numbers[i + 1];
+                   numbers[i + 1] = 0;
+                   newSize--;
+                 System.out.println(newSize);
+               }
+           }
+       }
+        return newSize;
     }
 }
