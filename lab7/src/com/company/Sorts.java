1296a973892d7d9b0233031f452c17380e896131
LHS Compsci <lhscompsci@lh-l703-15-7368.ci.lexington.ma.us>
2016-11-16 12:41:36 -0500

    done up to question 2

--- lab7/src/com/company/Sorts.java
+++ lab7/src/com/company/Sorts.java
@@ -4,23 +4,19 @@
 //  A collection of methods for sorting Arrays.
 //********************************************************************
 package com.company;
- public class Sorts
-{
+ public class Sorts {
     
 	private static int[] temp;
 	
 	public static int Insertion(int[] list) {
 		for (int n = 1; n < list.length; n++) {
 			// Save the next element to be inserted:
-			
 			int listTemp = list[n];
-			
 			// Going backward from list[n-1], shift elements to the
 			// right until you find an element list[i] <= aTemp:
-			
 			int i = n;
-			while (i > 0 && listTemp < list[i-1]) {
-				list[i] = list[i-1];
+			while (i > 0 && listTemp < list[i-1]) { // everytime it goes back through the list untill it reaches a part that is already correct
+				list[i] = list[i-1];				// this allows it to check for new errors caused by the first switch of the loop
 				i--;
 			}
 			
@@ -32,7 +28,6 @@ package com.company;
 		
         return 0;
     }
-    
     public static int Selection(int[] list) {
 		for (int n = list.length; n > 1; n--) {
 			// Find the index iMax of the largest element
@@ -41,7 +36,7 @@ package com.company;
 			int iMax = 0;
 			for (int i = 1; i < n; i++) {
 				if (list[i] > list[iMax])
-					iMax = i;
+					iMax = i; //it goes through the list looking for a largest value and once it finds it it writes it to a new loop
 			}
 			
 			swap (list, iMax, n-1);
@@ -54,7 +49,6 @@ package com.company;
 		}
         return 0;
     }
-
     public static int Merge(int[] list, int from, int middle, int to) {
 		temp = new int[list.length];
 		int i = from, j = middle + 1, k = from;
@@ -71,6 +65,7 @@ package com.company;
 			}
 			k++;
 		}
+		//the above section sorts each half
 		
 		// Copy the tail of the first half, if any, into temp:
 		while (i <= middle) {
@@ -91,8 +86,8 @@ package com.company;
 			list[k] = temp[k];
 		return 0;
     }
-	
 	public static int mergeSort(int[] list, int from, int to) {
+		// this sort individually sorts two halves of the list and then combines them by comparing the lements of each sequantially.
 		if (to - from < 2) {       // Base case: 1 or 2 elements
 			if (to > from && list[to] < list[from]) {
 				int listTemp = list[to];  // swap a[to] and a[from]
@@ -108,11 +103,10 @@ package com.company;
 		}
 		return 0;
 	}
-	
     public static int QuickSort(int[] list, int from, int to) {
 		if (from >= to)
 			return 0;
-		
+		//take a center value and use it as a pivot putting everything lower and higher (hence pivot) repeat on fromed sections recusvily untill sorting is achieved
 		// Choose pivot list[p]:
 		int p = from;
 		// The choice of the pivot location may vary:
@@ -121,7 +115,8 @@ package com.company;
 		
 		// Partition:
 		
-		int i = from;
+		int i = from; // it uses the pivot point to compare all values to, and places
+					  // those that are smaller switch with the first value of the uncorrected list, thi socntniues until every value has been a pivot
 		int j = to;
 		while (i <= j) {
 			if (list[i] <= list[p])
@@ -152,21 +147,17 @@ package com.company;
 		
 		return 0;
 	}
-    
     public static int QuickMid(int[] list) {
     
 		return 0;
     }
-    
 	public static int QuickRandom(int[] list) {
     
 		return 0;
     }
-    
     private static void swap (int[] list, int a, int b) {
         int temp = list[a];
 		list[a] = list[b];
 		list[b] = temp;
     }
-	
 }
