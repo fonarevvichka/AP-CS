49668f06aaae1239eed16362a930962f5e60c2c6
LHS Compsci <lhscompsci@LH-L703-15-7368.local>
2016-12-01 14:26:08 -0500

    recursive part written not working

--- Lab8/src/com/company/Main.java
+++ Lab8/src/com/company/Main.java
@@ -9,19 +9,24 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner cin = new Scanner(System.in);
         int[] array = generateArray();
+        int[] results = new int[2];
+        results[0] = 1;
         print(array, false);
         System.out.println("\n");
         Arrays.sort(array);
         print(array, true);
         System.out.println("\n");
-
-        System.out.println("What item would you like to search for? ");
-        int searchQuery = cin.nextInt();
-        int location = Arrays.binarySearch(array, searchQuery);
-        if (location >= 0 )
-            System.out.println("status: found at index: " + location);
-        else
-            System.out.println("status: not found: " + location);
+//        for(int i = 0; i < 2; i++) {
+//            System.out.println("What item would you like to search for? ");
+//            int searchQuery = cin.nextInt();
+//            int location = Arrays.binarySearch(array, searchQuery);
+//            if (location >= 0)
+//                System.out.println("status: found at index: " + location);
+//            else
+//                System.out.println("status: not found: " + location);
+//        }
+        binarySearch(array, 4, results);
+        print(array, false);
     }
     public static int[] generateArray() {
         Random randomizer = new Random();
@@ -43,25 +48,27 @@ public class Main {
                 System.out.print(array[i-1] + " ");
         }
     }
-    @Override
-    public static int[] binarySearch(int[] array, int searchQuery) {
-        int low = 0;
-        int high = array.length - 1;
-        int[] results = new int[2];
-
-        while (low <= high) {
-            int mid = (low + high) >>> 1;
-            int midVal = array[mid];
-
-            if (midVal < searchQuery)
-                low = mid + 1;
-            else if (midVal > searchQuery)
-                high = mid - 1;
-            else
-                low = 200; // key found
+    public static int[] binarySearch(int[]array, int searchQuery, int[] results) {
+        results = binarySearch(array, searchQuery, 0, array.length/2, array.length-1, results);
+        return results;
+    }
+    public static int[] binarySearch(int[] array, int searchQuery, int lowVal, int midVal, int highVal, int[] results) {
+        if (lowVal == highVal) {
+            results[1]++;
+            results[0] = -1;
         }
-       if(low == 200) {
-           results[0] = 
+        else if (array[midVal] < searchQuery) {
+            results[1]++;
+            return binarySearch(array, searchQuery, midVal + 1, midVal + (highVal - midVal) /2, highVal, results);
        }
+       else if (array[midVal] > searchQuery){
+            results[1] += 2;
+            return binarySearch(array, searchQuery, lowVal, midVal - (highVal - midVal) /2, midVal - 1, results);
+        } else {
+            results[1] += 3;
+            results[0] = midVal;
+            return results;
+        }
+    return results;
     }
 }
