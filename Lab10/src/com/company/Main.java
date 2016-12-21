2ffdfa99267bbee4ea0e2b4720e8e12306746fa3
LHS Compsci <lhscompsci@lh-l703-15-7368.ci.lexington.ma.us>
2016-12-21 13:30:57 -0500

    detection works, need to add write to file, and asking for letter suggestions

--- Lab10/src/com/company/Main.java
+++ Lab10/src/com/company/Main.java
@@ -14,6 +14,7 @@ public class Main {
         File file2 = new File(pathname2);
         Scanner reader = null;
         Scanner readerTwo = null;
+        Scanner readerThree = null;
 
         //------------------ Open File ----------------------//
         try {
@@ -27,6 +28,13 @@ public class Main {
             readerTwo = new Scanner(file2);
         } catch (FileNotFoundException ex){
             System.out.println("could not open " + pathname2);
+            System.exit(1);
+        }
+
+        try {
+            readerThree = new Scanner(file);
+        } catch (FileNotFoundException ex){
+            System.out.println("could not open " + pathname);
             System.exit(1);
         }
 
@@ -61,9 +69,41 @@ public class Main {
             System.out.println(letters[i].getLetter() + " has a frequency of " + letters[i].getFrequency());
         //------------------ Sort and Print------------------//
 
-        //------------ Char array from Replace.txt ----------//
 
         //------------ Char array from Replace.txt ----------//
+        char[] replace = new char[26];
+        for(int k = 0; k < 26; k++) {
+            String tempWord = readerTwo.next();
+            replace[k] = tempWord.charAt(0);
+        }
+        //------------ Char array from Replace.txt ----------//
 
+        //------------------ Set Associated Letter ----------//
+        for (int i = 0; i < 26; i++) {
+            letters[i].setAssociatedLetter(replace[i]);
+        }
+        //------------------ Set Associated Letter ----------//
+            for (int i = 0; i < 26; i++) {
+                System.out.println(letters[i].getLetter() + " has a frequency of " + letters[i].getFrequency() + " and is associted to " + letters[i].getAssociatedLetter() + " i is " + i);
+            }
+//        System.out.print(letters[23].getAssociatedLetter());
+        //------------------ Print Decoded Message ----------//
+
+        while(readerThree.hasNext()) {
+            String cWord = readerThree.next();
+            cWord.toUpperCase();
+            for(int k = 0; k < cWord.length(); k++) {
+                if (cWord.charAt(k) > 64 && cWord.charAt(k) < 91) {
+                    for(int i = 0; i < 26; i++) {
+                        if (letters[i].getLetter() == cWord.charAt(k)) {
+                            System.out.print(letters[i].getAssociatedLetter());
+                        }
+                    }
+                } else {
+                    System.out.print(cWord.charAt(k));
+                }
+            }
+        }
+        //------------------ Print Decoded Message ----------//
     }
 }
