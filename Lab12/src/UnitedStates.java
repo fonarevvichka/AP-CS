3bc67376a618b4753cebe899c32f1dd76eff8452
LHS Compsci <lhscompsci@lh-l703-15-7368.ci.lexington.ma.us>
2017-01-26 14:26:12 -0500

    almost done

--- Lab12/src/UnitedStates.java
+++ Lab12/src/UnitedStates.java
@@ -1,61 +1,128 @@
-import com.sun.tools.hat.internal.model.StackTrace;
-import com.sun.tools.javac.util.List;
+// Vichka Fonarev
+// F Block
+// Lab 12
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Created by lhscompsci on 1/23/17.
  */
-public class UnitedStates{
-    public static void main(String[] Args){
+public class UnitedStates {
+    public static void main(String[] Args) {
         File states = new File("states.txt");
         ArrayList<String> statesList = new ArrayList<String>();
         Scanner cin = null;
-        int counter = 0, insertPosition;
+        int counter = 0, insertPosition, duplicate = 0;
         String pWord, cWord;
 
         try {
-            cin =  new Scanner(states);
+            cin = new Scanner(states);
         } catch (FileNotFoundException ex) {
             System.out.println("file not found exception" + ex);
             System.exit(1);
         }
 
         while (cin.hasNextLine()) {
-           if (counter == 0) {
-               statesList.add(counter, cin.next());
-           } else {
-               insertPosition = counter;
+            if (counter == 0) {
+                cWord = cin.nextLine();
+                if (cWord.equals(null)) {
+                    counter--;
+                } else {
+                    statesList.add(counter, cin.nextLine());
+                }
+            } else {
+                insertPosition = counter - duplicate;
 
-               cWord = cin.nextLine();
-               if(insertPosition == 1) {
-                   pWord = "a";
-//                   System.out.println(pWord);
-               } else {
-                   pWord = statesList.get(insertPosition-1);
-               }
+                cWord = cin.nextLine();
+                pWord = statesList.get(insertPosition - 1);
 
-               System.out.println(cWord.compareToIgnoreCase(pWord) > 0);
-               while (cWord.compareToIgnoreCase(pWord) > 0) {
-                   System.out.println(pWord);
-                   System.out.println(cWord);
-//                   System.out.println(insertPosition);
-                   insertPosition--;
+                while (cWord.compareToIgnoreCase(pWord) < 0) {
+                    insertPosition--;
+                    if (insertPosition == 0)
+                        break;
+                    pWord = statesList.get(insertPosition - 1);
+                }
+                if (!isInList(statesList, cWord)) {
+                    statesList.add(insertPosition, cWord);
+                } else {
+                    duplicate++;
+                }
+            }
+            counter++;
+        }
+        displayList(statesList);
+        insertItem(statesList, "Hawaii");
+//        System.out.println("\n");
+//        displayList(statesList);
+//        System.out.println("\n");
+//        removeItem(statesList, "arizasdfona");
+        displayList(statesList);
+    }
 
-                   pWord = statesList.get(insertPosition-1);
-               }
-               statesList.add(insertPosition, cWord);
-           }
-           counter++;
+
+    public static void insertItem(ArrayList<String> statesList, String cWord) {
+        int insertPosition = statesList.size();
+        if (statesList.size() == 0) {
+            statesList.add(0, cWord);
+        } else {
+            if (!isInList(statesList, cWord)) {
+                String pWord = statesList.get(insertPosition - 1);
+                if (cWord.compareToIgnoreCase(pWord) != 0) {
+                    while (cWord.compareToIgnoreCase(pWord) < 0) {
+                        insertPosition--;
+                        if (insertPosition == 0)
+                            break;
+                        pWord = statesList.get(insertPosition - 1);
+                    }
+                    statesList.add(insertPosition, cWord);
+                }
+            }
+        }
+    }
+
+    public void saveList(ArrayList<String> stateList) {
+        File outputFile = new File("output.txt");
+        PrintWriter writer = null;
+
+        try {
+            writer = new PrintWriter(outputFile);
+        } catch (IOException ex) {
+            System.out.println("could not write to file");
         }
 
+        for (String state : stateList) {
+            try {
+                writer.println(state);
+            } catch (Exception x) {
+                System.out.println("couldn't write to output file");
+            }
+            writer.print("\n");
+        }
+        writer.flush();
+    }
 
-
-        for (int i = 0; i < statesList.size(); i++) {
-            System.out.println(statesList.get(i) + " " + i);
+    public static void removeItem (ArrayList<String> stateList, String state) {
+        for(int i = 0; i < stateList.size(); i++) {
+            if (isInList(stateList, state)) {
+                stateList.remove(i);
+            }
+        }
+    }
+    public static boolean isInList(ArrayList <String> stateList, String word) {
+        for (int i = 0; i < stateList.size(); i++) {
+            if(word.equalsIgnoreCase(stateList.get(i)))
+                return  true;
+        }
+        return false;
+    }
+    public static void displayList(ArrayList<String> statesList) {
+        for(String state : statesList) {
+            System.out.println(state);
         }
     }
 }
